9089c23d927f1fe9c7a33f41a6d95724579b9ac7
hassan-marz <100999726+hassan-marz@users.noreply.github.com>
2022-03-13 12:03:14 +0400

    Update movecommand_frw.py

--- movecommand_frw.py
+++ movecommand_frw.py
@@ -5,17 +5,17 @@ from pprint import pprint
 from can.message import Message
 
 #loading the dbc
-db = cantools.db.load_file('/home/hassan/Desktop/EZmile/EZ10_test_LMS.dbc') #change this line to the file path on your device
+db = cantools.db.load_file('/home/hassan/Desktop/EZmile/EZ10_test_LMS.dbc')
 
 #print(db)
 
 msg = db.get_message_by_name('PC_To_Var_1238')
 #print(msg)
-msg_data = msg.encode({'Command_Accel':1.0,'Command_Speed':1.0,'Front_Steer_Command':0.0,'Back_Steer_Command':0.0})
+msg_data = msg.encode({'Command_Accel':0.3,'Command_Speed':0.2,'Front_Steer_Command':0.0,'Back_Steer_Command':0.0})
 
 msg3 = can.Message(arbitration_id=0x214,data=[232, 3, 208, 7, 0, 0, 0, 0],is_extended_id=False)
-print(msg_data)
-print(msg3)
+#print(msg_data)
+#print(msg3)
 
 #sending the message
 bus = can.interface.Bus(bustype='socketcan',channel='slcan0', bitrate=500000)
@@ -24,9 +24,8 @@ msg = can.Message(arbitration_id=msg.frame_id,data=msg_data,is_extended_id=False
 while True:
 	try:
     		bus.send(msg)
+    		time.sleep(0.02)
     		print("message sent on {}".format(bus.channel_info))
-    		time.sleep(0.005)
     		#print(msg)
-    
 	except can.CanError:
     		print("Message NOT sent")
