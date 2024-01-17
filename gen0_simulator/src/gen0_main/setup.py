5ead8377b60387d4a61af23b301c8ba20df77fab
Riyadh <alkharrat.riyadh@gmail.com>
2024-01-17 12:05:04 +0400

    SDF file collision update

--- gen0_simulator/src/gen0_main/setup.py
+++ gen0_simulator/src/gen0_main/setup.py
@@ -8,26 +8,42 @@ package_name = 'gen0_main'
 # Iterate through all the files and subdirectories
 # to build the data files array
 
+data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ]
+
+
+def package_files(data_files, directory_list):
+
+    paths_dict = {}
+
+    for directory in directory_list:
+
+        for (path, directories, filenames) in os.walk(directory):
+
+            for filename in filenames:
+
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-        glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'urdf'),
-        glob(os.path.join('urdf', '*.*'))),
-        (os.path.join('share', package_name, 'meshes'),
-        glob(os.path.join('meshes', '*.*'))),
-        (os.path.join('share', package_name, 'worlds/san_parking_model'),
-        glob(os.path.join('worlds/**/*.*'))),
-        (os.path.join('share', package_name, 'config'),
-        glob(os.path.join('config', '*.*'))),
-        
-    ],
+    data_files= package_files(data_files, ['launch/', 'worlds/', 'config/', 'urdf/', 'meshes/']),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='av-lab',
